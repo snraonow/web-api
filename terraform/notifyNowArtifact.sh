eda470ba9513f902d476694271eb8afdb0cc26b9
santosh.nagaraja <santosh.nagaraja@servicenow.com>
2023-06-30 10:39:17 -0700

    artifact notification

---
+++ terraform/notifyNowArtifact.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+STEP_NAME=$1
+# RESULT=$3  #use result to specify status?
+DATE=$(date +%Y-%m-%d" "%H:%M:%S)
+ARTIFACT_NAME="docker-image-name"
+ARTIFACT_REPO="docker-repo-name"
+ARTIFACT_VERSION="dockerImageTag"
+ARTIFACT_SEMANTIC_VERSION=""
+URL=https://$INSTANCE_NAME.service-now.com/api/sn_devops/devops/tool/orchestration
+
+# https://docs.env0.com/docs/custom-flows#exposed-env0-system-environment-variables All environment variables
+
+echo "STEP_NAME : $STEP_NAME , UPSTREAM_STEP_NAME : $UPSTREAM_STEP_NAME , RESULT : $RESULT , TOOL_ID : $TOOL_ID "
+echo "Artifact notification invoked to $URL $URL?orchestrationToolId=$TOOL_ID "
+
+ARTIFACT_DATA="{
+    \"taskExecutionNumber\": \"$ENV0_DEPLOYMENT_LOG_ID\",
+    \"pipelineName\": \"$ENV0_PROJECT_NAME\",
+    \"stageName\": \"$STEP_NAME\",
+    \"artifacts\": [
+        {
+            \"name\": \"$ARTIFACT_NAME\",
+            \"repositoryName\": \"$ARTIFACT_REPO\",
+            \"semanticVersion\": \"$ARTIFACT_SEMANTIC_VERSION",
+            \"version\": \"$ARTIFACT_VERSION\"
+        }
+    ]
+}"
+
+
+echo "Artifact Data $ARTIFACT_DATA" 
+
+curl -X POST -H "Content-Type: application/json" -u "$USERNAME:$PASSWORD" $URL?orchestrationToolId=$TOOL_ID -d "$ARTIFACT_DATA"
+
