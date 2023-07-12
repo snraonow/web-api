e4098dec9aadef283f8c0df1fd18f5102b1a4b4e
sannrao <81649076+sannrao@users.noreply.github.com>
2023-07-12 14:45:11 +0530

    upstreamid

--- terraform/notifyNow.sh
+++ terraform/notifyNow.sh
@@ -16,11 +16,11 @@ echo "Webhook notification invoked to $URL $URL?toolId=$TOOL_ID "
 
 if [[ -z "$UPSTREAM_STEP_NAME" ]]; then
    # $var is empty, do what you want
-   UPSTREAM_ID=\"\"
-   UPSTREAM_TASK_URL=\"\"
+   UPSTREAM_ID=''
+   UPSTREAM_TASK_URL=''
 else
-  UPSTREAM_ID=\"$ENV0_ENVIRONMENT_NAME#$UPSTREAM_STEP_NAME/$ENV0_DEPLOYMENT_LOG_ID\"
-  UPSTREAM_TASK_URL=\"api.env0.com/p/$ENV0_PROJECT_ID/environments/$ENV0_ENVIRONMENT_ID/$UPSTREAM_STEP_NAME/deployments/$ENV0_DEPLOYMENT_LOG_ID\"
+  UPSTREAM_ID=$ENV0_ENVIRONMENT_NAME#$UPSTREAM_STEP_NAME/$ENV0_DEPLOYMENT_LOG_ID
+  UPSTREAM_TASK_URL=api.env0.com/p/$ENV0_PROJECT_ID/environments/$ENV0_ENVIRONMENT_ID/$UPSTREAM_STEP_NAME/deployments/$ENV0_DEPLOYMENT_LOG_ID
 fi
 
 UPSTREAM_ID= 
@@ -41,8 +41,8 @@ WEBHOOK_DATA="{
   \"result\": \"$RESULT\",
   \"startDateTime\": \"$DATE\",
   \"endDateTime\":\"$DATE\",
-  \"upstreamId\": $UPSTREAM_ID,
-  \"upstreamTaskUrl\": $UPSTREAM_TASK_URL
+  \"upstreamId\": \"$UPSTREAM_ID\",
+  \"upstreamTaskUrl\": \"$UPSTREAM_TASK_URL\"
 },
 \"orchestrationTask\": {
   \"orchestrationTaskURL\": \"api.env0.com/p/$ENV0_PROJECT_ID/environments/$ENV0_ENVIRONMENT_ID/$STEP_NAME\",
